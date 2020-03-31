24fe6b39d9f16e533dba7653de60b32387e3bbcd
Marc Elrick <marcelrick15@hotmail.com>
2020-03-31 21:11:24 +0100

    Modified initial project

---
+++ lib/views/titleScreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TitleScreen extends StatefulWidget {
+  TitleScreen({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _TitleScreenState createState() => _TitleScreenState();
+}
+
+class _TitleScreenState extends State<TitleScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
